a1abd612b47840390d3fcbafe614b7293b9c832e
MaMunoz99 <mmunozo@estudiantesunap.cl>
2022-11-17 00:08:52 -0300

    se hizo CASI todo el codigo arreglar funcion agregar id poll

--- Proyecto/DataBase/conexion.py
+++ Proyecto/DataBase/conexion.py
@@ -1,5 +1,4 @@
 import mysql.connector
-
 from mysql.connector import Error
 
 class DAO():
@@ -10,57 +9,250 @@ class DAO():
                 host='localhost',
                 port=3306,
                 user='root',
-                ##password='123456',
-                db='proyecto1'
+                db='FLP'
             )
         except Error as ex:
             print("Error al intentar la conexión: {0}".format(ex))
 
-    def arrayquestions(self):
+    def listadmin(self):
         if self.conexion.is_connected():
             try:
                 cursor = self.conexion.cursor()
-                cursor.execute("SELECT * FROM preguntas")
-                result = cursor.fetchall()
-                return result
+                cursor.execute("select run,password, name_us from users;")
+                resultados = cursor.fetchall()
+                return resultados
+            except Error as ex:
+                print("Error al intentar la conexión: {0}".format(ex))    
+    
+    def conectionloginadmin(self,curso):
+        if self.conexion.is_connected():
+            try:
+                cursor = self.conexion.cursor()
+                sql = "select *from users where run = {0} and password = '{1}';"
+                cursor.execute(sql.format(curso[0], curso[1]))
+                self.conexion.commit()
             except Error as ex:
                 print("Error al intentar la conexión: {0}".format(ex))
 
+    def listpatients(self):
+        if self.conexion.is_connected():
+            try:
+                cursor = self.conexion.cursor()
+                cursor.execute("select run,password, name_pa from patients;")
+                resultados = cursor.fetchall()
+                return resultados
+            except Error as ex:
+                print("Error al intentar la conexión: {0}".format(ex)) 
+    
+    def conectionloginpatients(self,curso):
+        if self.conexion.is_connected():
+            try:
+                cursor = self.conexion.cursor()
+                sql = "select *from patients where run = {0} and password = '{1}';"
+                cursor.execute(sql.format(curso[0], curso[1]))
+                self.conexion.commit()
+            except Error as ex:
+                print("Error al intentar la conexión: {0}".format(ex))    
+    
+    def arrayquestions(self):
+        if self.conexion.is_connected():
+            cursor = self.conexion.cursor()
+            cursor.execute("Select *from questions where DELETED_AT=False;")
+            result = cursor.fetchall()
+            return result
+
+    def arraypatients(self):
+        if self.conexion.is_connected():
+            cursor = self.conexion.cursor()
+            cursor.execute("select idpatient,run,dv,NAME_PA,FATHER_NAME,MOTHER_NAME,GENDER,BIRTHDAY,OBSERVATIONS from patients where DELETED_AT = 0;")
+            result = cursor.fetchall()
+            return result
+           
     def addquestions(self, questions):
         if self.conexion.is_connected():
             try:
                 cursor = self.conexion.cursor()
-                sql = "INSERT INTO preguntas (IDPREGUNTAS, DES, VAL) VALUES ('{0}', '{1}', {2})"
-                cursor.execute(sql.format(questions[0], questions[1], questions[2]))
+                sql = "insert into QUESTIONS(idtest,questions,descriptions,deleted_at) values (NULL,'{0}','{1}',False);"
+                cursor.execute(sql.format(questions[0], questions[1]))
                 self.conexion.commit()
                 print("¡Pregunta registrada!\n")
             except Error as ex:
                 print("Error al intentar la conexión: {0}".format(ex))
 
-
-    def sum_respuestas(self):
+    def updatequestion(self, info):
         if self.conexion.is_connected():
             try:
                 cursor = self.conexion.cursor()
-                cursor.execute("select SUM(RESPUESTA) from relationship_1;")
-                result = cursor.fetchall()
-                return result
+                sql = "update questions set questions = '{0}', descriptions = '{1}' where idquestion = {2};"
+                cursor.execute(sql.format(info[0], info[1], info[2]))
+                self.conexion.commit()
+                print("Pregunta actualizada!\n")
             except Error as ex:
                 print("Error al intentar la conexión: {0}".format(ex))
 
-    def sum_preguntas(self):
+    def delquestion(self, info):
         if self.conexion.is_connected():
             try:
                 cursor = self.conexion.cursor()
-                cursor.execute("select SUM(VAL) from preguntas;")
-                result = cursor.fetchall()
-                return result
+                sql = "update questions set DELETED_AT = {0} where idquestion = {1}; "
+                cursor.execute(sql.format(info[0],info[1]))
+                self.conexion.commit()
+                print("¡Pregunta eliminada!\n")
             except Error as ex:
                 print("Error al intentar la conexión: {0}".format(ex))
 
+    def delpatients(self, info):
+        if self.conexion.is_connected():
+            try:
+                cursor = self.conexion.cursor()
+                sql = "update patients set DELETED_AT = {0} where idpatient = {1}; "
+                cursor.execute(sql.format(info[0],info[1]))
+                self.conexion.commit()
+                print("¡Pasiente eliminado!\n")
+            except Error as ex:
+                print("Error al intentar la conexión: {0}".format(ex))   
+
+    def addpatients(self, questions):
+        if self.conexion.is_connected():
+            try:
+                cursor = self.conexion.cursor()
+                sql = "insert into patients (run,dv,NAME_PA,FATHER_NAME,MOTHER_NAME,GENDER,BIRTHDAY,PASSWORD,OBSERVATIONS,DELETED_AT) values ({0},'{1}','{2}','{3}','{4}','{5}','{6}','{7}',NULL,0);"
+                cursor.execute(sql.format(questions[0], questions[1], questions[2], questions[3], questions[4], questions[5], questions[6], questions[7]))
+                self.conexion.commit()
+                print("¡Pregunta registrada!\n")
+            except Error as ex:
+                print("Error al intentar la conexión: {0}".format(ex))
+
+    def updatepatients(self, info):
+        if self.conexion.is_connected():
+            try:
+                cursor = self.conexion.cursor()
+                sql = "update questions set questions = '{0}', descriptions = '{1}' where idquestion = {2};"
+                cursor.execute(sql.format(info[0], info[1], info[2]))
+                self.conexion.commit()
+                print("Pregunta actualizada!\n")
+            except Error as ex:
+                print("Error al intentar la conexión: {0}".format(ex))
+
+    def updatepatients(self, info):
+        if self.conexion.is_connected():
+            try:
+                cursor = self.conexion.cursor()
+                sql = "update patients set password = '{0}' where idpatient = {1};"
+                cursor.execute(sql.format(info[0], info[1]))
+                self.conexion.commit()
+                print("Pasiente actualizado!\n")
+            except Error as ex:
+                print("Error al intentar la conexión: {0}".format(ex))    
+
+    def updateobservationpatients(self, info):
+        if self.conexion.is_connected():
+            try:
+                cursor = self.conexion.cursor()
+                sql = "update patients set OBSERVATIONS = '{0}' where idpatient = {1};"
+                cursor.execute(sql.format(info[0], info[1]))
+                self.conexion.commit()
+                print("Pasiente actualizado!\n")
+            except Error as ex:
+                print("Error al intentar la conexión: {0}".format(ex))  
+
+
+    def printtest(self): 
+        if self.conexion.is_connected():
+            cursor = self.conexion.cursor()
+            cursor.execute("Select *from tests where DELETED_AT=False;")
+            result = cursor.fetchall()
+            return result 
+
+    def addtest(self, test):
+        if self.conexion.is_connected():
+            try:
+                cursor = self.conexion.cursor()
+                
+                sql = "insert into tests (name,cut_point,max_point,observations,deleted_at) values ('{0}',{1},{2},'{3}',False);"
+                cursor.execute(sql.format(test[0], test[1], test[2], test[3]))
+                self.conexion.commit()
+                print("¡Test registrado!\n")
+            except Error as ex:
+                print("Error al intentar la conexión: {0}".format(ex))
     
+    def deltest(self, info):
+        if self.conexion.is_connected():
+            try:
+                cursor = self.conexion.cursor()
+                sql = "update tests set DELETED_AT = {0} where idtest = {1}; "
+                cursor.execute(sql.format(info[0],info[1]))
+                self.conexion.commit()
+                print("¡Test eliminado!\n")
+            except Error as ex:
+                print("Error al intentar la conexión: {0}".format(ex))
 
 
+    def printtestpatient(self): 
+        if self.conexion.is_connected():
+            cursor = self.conexion.cursor()
+            cursor.execute("select idtest,name,observations from tests where deleted_at = 0;")
+            result = cursor.fetchall()
+            return result 
 
-
+    def printquestionspatient(self): 
+        if self.conexion.is_connected():
+            cursor = self.conexion.cursor()
+            cursor.execute("select idquestion,questions,descriptions,tests.idtest,tests.name from questions join tests on questions.idtest = tests.idtest;")
+            result = cursor.fetchall()
+            return result    
     
+    def addaswerd(self, test):
+        if self.conexion.is_connected():
+            try:
+                cursor = self.conexion.cursor()
+                sql = "insert into answers (idquestion,points,text,idpoll) values ({0},{1},'{2}',{3});"
+                cursor.execute(sql.format(test[0], test[1], test[2], test[3]))
+                self.conexion.commit()
+                print("¡Respuesta registrada!\n")
+            except Error as ex:
+                print("Error al intentar la conexión: {0}".format(ex))  
+
+    def printmaxpoll(self): 
+        if self.conexion.is_connected():
+            cursor = self.conexion.cursor()
+            cursor.execute("select  MAX(idpoll) from poll;")
+            result = cursor.fetchall()
+            return result 
+
+    def addidpoll(self,test):
+        if self.conexion.is_connected():
+            try:
+                cursor = self.conexion.cursor()
+                sql ="insert into poll (idpoll) value ({0});"
+                cursor.execute(sql(test[0]))
+                self.conexion.commit()
+                print("¡pol agregada!\n")
+            except Error as ex:
+                print("Error al intentar la conexión: {0}".format(ex))
+
+
+
+
+    #def arrayquestionstest():
+
+
+    #def sum_respuestas(self):
+    #    if self.conexion.is_connected():
+    #        try:
+    #            cursor = self.conexion.cursor()
+    #            cursor.execute("select SUM(RESPUESTA) from relationship_1;")
+    #            result = cursor.fetchall()
+    #            return result
+    #        except Error as ex:
+    #            print("Error al intentar la conexión: {0}".format(ex))
+
+    #def sum_preguntas(self):
+    #    if self.conexion.is_connected():
+    #        try:
+    #            cursor = self.conexion.cursor()
+    #            cursor.execute("select SUM(VAL) from preguntas;")
+    #            result = cursor.fetchall()
+    #            return result
+    #        except Error as ex:
+    #            print("Error al intentar la conexión: {0}".format(ex))
--- Proyecto/funciones.py
+++ Proyecto/funciones.py
@@ -1,53 +1,262 @@
 from multiprocessing.sharedctypes import Value
+from DataBase.conexion import DAO
 
+dao = DAO()
+
+def listadmins(info):
+    for cur in info:
+        datos = "{0} {1} {2}"
+        datos.format(cur[0], cur[1], cur[2])
+
+
+def verificationloginadmin(info):
+    flag = False
+    run = int(input("RUN: "))
+    password = input("PASSWORD: ")
+    for cur in info:
+        if (cur[0] == run) and (cur[1]==password):
+            flag = True
+            break
+    if flag == True:
+        print("Bienvenido ",cur[2])
+        return True  
+    else:
+        print("No estas Registrado") 
+        return False
+
+
+def listpatients(info):
+    for cur in info:
+        datos = "{0} {1} {2}"
+        datos.format(cur[0], cur[1], cur[2])
+
+def verificationloginpatients(info):
+    flag = False
+    nameuser = ''
+    run = int(input("RUN: "))
+    password = input("PASSWORD: ")
+    for cur in info:
+        if (cur[0] == run) and (cur[1]==password):
+            flag = True
+            break
+    if flag == True:
+        print("Bienvenido",cur[2])
+        return True   
+    else:
+        print("No estas Registrado")  
+        return False   
 
 def arrayquestions(questions):
     print("\nPreguntas: \n")
     for cur in questions:
-        info = "ID: {0} | Description: {1} ({2} Value)"
+        info = "ID: {0} | IDTEST: {1} | Pregunta: '{2}' | Descripcion: '{3}'"
+        print(info.format(cur[0], cur[1], cur[2],cur[3]))
+    print(" ")
+
+def arraypatients(patients):
+    print("\nPasientes: \n")
+    for cur in patients:
+        info = "ID: {0} | RUN: {1} | DIGITO VERIFICADOR: '{2}' | NOMBRES: '{3}' | APELLIDO PATERNO: '{4}'  | APELLIDO MATERNO: '{5}' | GENERO: '{6}' | FECHA NACIMIENTO '{7}' | OBSERVACION '{8}'" 
+        print(info.format(cur[0], cur[1], cur[2],cur[3],cur[4],cur[5],cur[6],cur[7],cur[8]))
+    print(" ")
+
+def arrayobservationpatients(patients):
+    print("\nPasientes: \n")
+    for cur in patients:
+        info = "ID: {0} | RUN: {1} | DIGITO VERIFICADOR: '{2}' | NOMBRES: '{3}' | APELLIDO PATERNO: '{4}'  | APELLIDO MATERNO: '{5}' | GENERO: '{6}' | FECHA NACIMIENTO '{7}' | OBSERVACION '{8}'" 
+        print(info.format(cur[0], cur[1], cur[2],cur[3],cur[4],cur[5],cur[6],cur[7],cur[8]))
+    print(" ")
+
+
+def addquestion():
+    questions = input("Ingrese una pregunta: ")
+    description = input("Descripcion para la pregunta: ")
+    add = (questions, description)
+    return add
+
+def Updatequestion(info):
+    arrayquestions(info)
+    id = int(input("Ingrese el código de la pregunta a editar: "))
+    questions = input("Ingrese una pregunta: ")
+    description = input("Descripcion para la pregunta: ") 
+    for cur in info:
+        if cur[0] == id:
+            add=(questions,description,id)
+            return add
+
+def deletequestion(info):
+    arrayquestions(info)
+    id = int(input("Ingrese el código de la pregunta a eliminar: "))
+    borrado = 1
+    for cur in info:
+        if cur[0] == id:
+            delete = (borrado,id)
+            return delete
+
+def deletepatients(info):
+    arraypatients(info)
+    id = int(input("Ingrese el código del pasiente a eliminar: "))
+    borrado = 1
+    for cur in info:
+        if cur[0] == id:
+            delete = (borrado,id)
+            return delete
+
+def addpatients():
+    run = int(input("Ingrese el run del pasiente: "))
+    dv = input("Ingrese el digito verificador: (Numeros del 1 al 9 o k)")
+    name_pa = input("Ingrese el nombre: (Ejemplo: Diego Andres)")
+    father_nam = input("Ingrese el apellido paterno: ")
+    mother_nam = input("Ingrese el apellido materno: ")
+    gener = input("Ingrese el genero al que se identifica: ")
+    birthday = input("Ingrese su cumpleaños: (Ejemplo = DD/MM/YY)")
+    password= input("Ingrese su contraseña: ")
+
+
+    add = (run, dv,name_pa,father_nam,mother_nam,gener,birthday,password)
+    return add
+
+def Updatepatient(info):
+    listpatients(info)
+    id = int(input("Ingrese el código del pasiente a editar: "))
+    password = input("Ingrese una nueva contraseña: ") 
+    for cur in info:
+        if cur[0] == id:
+            add=(password,id)
+            return add
+
+def Updateobservationpatient(info):
+    listpatients(info)
+    id = int(input("Ingrese el código del pasiente a editar: "))
+    observation = input("Ingrese su diagnostico: ") 
+    for cur in info:
+        if cur[0] == id:
+            add=(observation,id)
+            return add
+
+def searchpatient(info):
+    flag = False
+    nameuser = ''
+    run = int(input("RUN: "))
+    dv = input("Digito Verificador: ")
+    for cur in info:
+        if (cur[1] == run) and (cur[2]==dv):
+            flag = True
+            break
+    if flag == True:
+        arrayobservationpatients(info)
+    else:
+        print("No estas Registrado o no estas Disponible")  
+
+
+def arraytest(tests):
+    print("\nTests: \n")
+    for cur in tests:
+        info = "ID: {0} | NAME: '{1}' | PUNTAJE DE CORTE: '{2}' | VALOR TEST(MAX): '{3}' | DESCRIPCION: '{4}'"
+        print(info.format(cur[0], cur[1], cur[2],cur[3],cur[4]))
+    print(" ")
+
+def addtest():
+    name = input("Ingrese el nombre del test: ")
+    cut_point = int(input("Ingrese el puntaje de corte: "))
+    max_point = int(input("Ingrese el puntaje max del test: "))
+    observations = input("Ingrese la descripcion del test: ")
+    add = (name,cut_point,max_point,observations)
+    return add
+
+def deltest(info):
+    arraytest(info)
+    id = int(input("Ingrese el código del pasiente a eliminar: "))
+    borrado = 1
+    for cur in info:
+        if cur[0] == id:
+            delete = (borrado,id)
+            return delete
+
+def arraytestpatient(tests):
+    print("\nTests: \n")
+    for cur in tests:
+        info = "ID: {0} NAME: '{1}' | DESCRIPCION: '{2}'"
         print(info.format(cur[0], cur[1], cur[2]))
     print(" ")
 
-
-def auth():
-    flagid = False
-    while(not flagid):
-        id = input("Ingrese código: ")
-        if len(id)<4:
-            flagid = True
+    option = int(input("Que test decea realizar: "))
+    for cur in tests:
+        if cur[0] == option:
+            print("estas haciendo el test numero ",cur[0])
+            return True
         else:
-            print("Código incorrecto: Debe tener hasta 4 dígitos.")
+            return False
 
-    description = input("Ingrese una pregunta: ")
+def arrayquestiontest(tests):
+    print("\nPreguntas: \n")
+    for cur in tests:
+        info = " '{0}' '{1}'  {2} '{3}'"
+        info.format(cur[0], cur[1], cur[2], cur[3])
+        print("PREGUNTA: ",cur[1], "| EXPLICACION: ",cur[2])
+        add = addanswerd(cur[0])
+        dao.addaswerd(add)
+        print(" ")
 
-    flagvalue = False
-    while(not flagvalue):
-        Value = input("Ingrese valor: ")
-        if Value.isnumeric():
-            if (int(Value) > 0 and int(Value) < 6):
-                flagvalue = True
-                Value = int(Value)
-            else:
-                print("Los créditos deben ser mayor a 0 y menor a 5.")
+
+def addanswerd(dato):
+        aswerd = int(input("Cual es su Respuesta(Ejemplo= 1): "))
+        text = input("Argumentar Respuesta (opcional): ")
+        poll= dao.printmaxpoll()
+        print(poll[0])
+        a = maxpoll(poll[0])
+
+        if a==None:
+            a = 1
         else:
-            print("Créditos incorrectos: Debe ser un número únicamente.")
+            a=a+1
+        add = (dato,aswerd,text,a)
+        
+        dao.addidpoll(add[3])
+        
+        return add
 
-    questions = (id, description, Value)
-    return questions
+            
+def maxpoll(tests):
+    dato = tests[0]
+    return dato
 
-def countquestions(questions):
-    print("\nValor: \n")
-    for cur in questions:
-        info = "num preguntas es {0} "
-        print(info.format(cur[0]))
-        suma = 0
-        suma = cur[0] / 2
-        print (suma)
-    print(" ")
+def idpoll(tests):
+    dato = tests[0]
+    return dato
 
-def test(questions):
-    print("\nValor: \n")
-    for cur in questions:
-        info = "{0}"
-        print(info.format(cur[0]))
-    print(" ")
+def verification(info):
+    flag = False
+    password = input("PASSWORD: ")
+    for cur in info:
+        if (cur[1]==password):
+            flag = True
+            break
+    if flag == True:
+        print("Confirmado")
+        return True   
+    else:
+        print("No esta Confirmada esta Encuesta")  
+        return False   
+
+def passwordprint(tests):
+    password = tests[0]
+    return password
+
+
+#def countquestions(questions):
+#    print("\nValor: \n")
+#    for cur in questions:
+#        info = "num preguntas es {0} "
+#        print(info.format(cur[0]))
+#        suma = 0
+#        suma = cur[0] / 2
+#        print (suma)
+#    print(" ")
+
+#def test(questions):
+#    print("\nValor: \n")
+#    for cur in questions:
+#        info = "{0}"
+#        print(info.format(cur[0]))
+#    print(" ")
